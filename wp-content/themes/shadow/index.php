51bb9e3b2c772ca922bd852e89e9e510c0de0128
Chris Wayne Comendador <cwayne.comendador@gmail.com>
2020-05-19 20:12:58 +0800

    Started working on personal introduction section

--- wp-content/themes/shadow/index.php
+++ wp-content/themes/shadow/index.php
@@ -25,21 +25,51 @@ get_header();
 
 	</section>
 
-	<section class="intro box">
+	<section class="intro box--padding">
+
+
 
 		<section class="intro__img">
 			
+
 			<div class="intro__circle">
 
+				<div class="circles-container">
+
+					<div class="circle__img">
+						<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_wordpress.png" " alt="Wordpress Logo">
+					</div>
+
+					<div class="circle__img">
+						<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_yoast.png" " alt="Yoast SEO Logo">
+					</div>
+					
+					<div class="circle__img">
+						<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_analytics.png" " alt="Google Analytics Logo">
+					</div>
+
+					<div class="circle__img">
+						<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_woo.png" " alt="WooCommerce Logo">
+					</div>
+
+					<div class="circle__img">
+						<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_w3.png" " alt="W3 Total Cache Logo">
+					</div>
+
+				</div>					
+
 			</div>
-			<img src="<?php echo bloginfo('template_url')?>/assets/images/logo_me.png" alt="Chris Wayne Comendador">
+
+			<img class="intro__profimage" src="<?php echo bloginfo('template_url')?>/assets/images/logo_me.png" alt="Chris Wayne Comendador">
 
 
 		
 		</section>
 
+
 		<section class="intro__description">
-			
+		
+		
 			<h3>I am a professional <span>wordpress theme developer</span> and a UX/UI designer.</h3>
 			
 			<p>
@@ -52,7 +82,11 @@ get_header();
 
 			<a href="<?php echo site_url();?>/cart" class="btn btn--primary">I need a website <i class="fa fa-caret-right"></i></a>
 		
+
 		</section>
+
+
+
 
 	</section>
 
